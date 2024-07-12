c257f24a90c25e486a811526f415c64e300a24e2
phantrunghieu91 <phantrunghieu1291@gmail.com>
2024-07-12 10:42:24 +0700

    Added browser-sync

--- gulpfile.mjs
+++ gulpfile.mjs
@@ -5,6 +5,7 @@ import { task, watch, src, parallel, dest } from 'gulp';
 import rename from 'gulp-rename';
 import sourcemaps from 'gulp-sourcemaps';
 import fancyLog from 'fancy-log';
+import browserSync from 'browser-sync';
 // Css related
 import * as dartSass from 'sass';
 import gulpSass from 'gulp-sass';
@@ -30,7 +31,22 @@ const paths = {
     dest: './dist/js',
     watch: 'src/ts/**/*.ts',
   },
+  php: {
+    watch: '**/*.php',
+  },
 };
+// Browser sync
+function initBrowserSync() {
+  browserSync.init({
+    open: false,
+    injectChanges: true,
+    // proxy: 'http://localhost:3000',  // Uncomment this line if you are using a local server
+  });
+}
+function reload(done) {
+  browserSync.reload();
+  done();
+}
 function compileScss() {
   return src(paths.scss.exports)
     .pipe(sourcemaps.init()) // Initialize sourcemaps before any transformations
@@ -53,7 +69,8 @@ function compileScss() {
         sourceRoot: `../../${paths.scss.src}`, // This is the path to the source file in the sourcemap
       })
     ) // Write sourcemaps after transformations, before saving the output
-    .pipe(dest(paths.scss.dest));
+    .pipe(dest(paths.scss.dest))
+    .pipe(browserSync.stream());
 }
 function bundleJS() {
   paths.ts.entries.map(entry => {
@@ -87,7 +104,8 @@ function bundleJS() {
             sourceRoot: `../../${paths.ts.src}`, // This is the path to the source file in the sourcemap
           })
         )
-        .pipe(dest(paths.ts.dest));
+        .pipe(dest(paths.ts.dest))
+        .pipe(browserSync.stream());
     }
     watchedBrowserify.on('update', reBundle);
     return reBundle();
@@ -95,10 +113,12 @@ function bundleJS() {
 }
 function watchFiles() {
   watch(paths.scss.watch, compileScss);
+  watch(paths.php.watch, { events: 'change' }, reload);
   bundleJS();
 }
 // Define gulp tasks
+task('browser-sync', initBrowserSync);
 task('style', compileScss);
 task('script', bundleJS);
 task('watch', watchFiles);
-task('default', parallel('style', 'script', 'watch'));
+task('default', parallel('browser-sync', 'style', 'script', 'watch'));
