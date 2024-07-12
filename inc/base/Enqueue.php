43c961c24d993a4e22f08cc5ce8ff7e00341bd1e
Hieu Phan Trung <102812781+phantrunghieu91@users.noreply.github.com>
2024-07-12 10:46:09 +0700

    Update Enqueue.php

--- inc/base/Enqueue.php
+++ inc/base/Enqueue.php
@@ -20,17 +20,9 @@ class Enqueue extends BaseController
 
   public function enqueueAdminsScripts()
   {
-    wp_enqueue_media();
-    wp_enqueue_style('jins-plugin-style', $this->plugin_url . 'assets/css/jins-plugin.min.css', [], null, 'all');
-    wp_enqueue_script('jins-plugins-script', $this->plugin_url . 'assets/js/jins-plugin.min.js', ['jquery'], null, true);
   }
 
   public function enqueueFrontEndScripts()
   {
-    // check if login_manager is activated
-    if ($this->isActivated('login_manager') && ! is_user_logged_in() ) {
-      wp_enqueue_style('jins-plugin-auth', $this->plugin_url . 'assets/css/jins-plugin-auth.min.css', [], null, 'all');
-      wp_enqueue_script('jins-plugins-auth', $this->plugin_url . 'assets/js/jins-plugin-auth.min.js', ['jquery'], null, true);
-    }
   }
 }
